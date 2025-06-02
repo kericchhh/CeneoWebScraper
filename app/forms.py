2af5669e5e53a5075a17b806225fec0774426447
Karim <karim.k2203@gmail.com>
2025-06-02 15:22:26 +0200

    02.06.2025

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
-from wtforms import Form,StringField, SubmitField, validators
+from wtforms import Form, StringField, SubmitField, validators
 
 class ExtractForm(Form):
-    product_id = StringField('Product_id',name='product_id',id='product_id',validators=[
+    product_id = StringField("Product id", name="product_id", id="product_id", validators=[
         validators.DataRequired(message="Product id is required"),
-        validators.Length(min=6,max=10,message="Product id should have between 6 and 10 characters"),
-        validators.Regexp(regexp='[0-9]+$',message="Product idd can contain ony digits")
+        validators.Length(min=6, max=10, message="Product id should have between 6 and 10 chracters"),
+        validators.Regexp(regex='^[0-9]+$', message="Product id can contain only digits")
     ])
-    submit = SubmitField('Extract opinions')
+    submit = SubmitField("Extract opinions")
